af60be8a7aec9ba50fd760cf02fdfaf253c3a893
unknown <abhirajrajput021@gmail.com>
2024-08-04 17:10:54 +0530

    first commit

---
+++ Largest Perimeter Triangle.java
@@ -0,0 +1,48 @@
+/* Given an integer array nums, return the largest perimeter of a triangle with a non-zero area, formed from three of these lengths. If it is impossible to form any triangle of a non-zero area, return 0.
+
+Example 1:
+
+Input: nums = [2,1,2]
+Output: 5
+Explanation: You can form a triangle with three side lengths: 1, 2, and 2.
+Example 2:
+
+Input: nums = [1,2,1,10]
+Output: 0
+Explanation: 
+You cannot use the side lengths 1, 1, and 2 to form a triangle.
+You cannot use the side lengths 1, 1, and 10 to form a triangle.
+You cannot use the side lengths 1, 2, and 10 to form a triangle.
+As we cannot use any three side lengths to form a triangle of non-zero area, we return 0.
+
+Constraints:
+
+3 <= nums.length <= 104
+1 <= nums[i] <= 106    */
+
+
+// Solution
+
+
+/* 
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);      // Sorteding in ascending order for find last is biggest element
+        int maxPerimeter = 0;
+
+        for(int i=n-1; i>=2; i--) {      // run roop Ulta because to store biggest element in side1
+            int side1 = nums[i];         // first biggest element in side1
+            int side2 = nums[i-1];       // second biggest in side2
+            int side3 = nums[i-2];       //and third biggest in side3
+
+            if(side1<side2+side3) {      // now checking side1 < side2 + side3 ex a+b>c if satisfy then store in perimeter
+                maxPerimeter = side1 + side2 + side3;
+                break;
+            }
+        }
+        return maxPerimeter;
+    }
+}
+
+ */
